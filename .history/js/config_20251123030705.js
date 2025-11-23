592fb3cf5b928f1bd8258f3d188349098f366b94
KeviNozh <kevintooresgarrido2435@gmail.com>
2025-11-23 03:07:20 -0300

    update45

--- .history/js/config_20251123030705.js
+++ .history/js/config_20251123030705.js
@@ -1,7 +1,8 @@
 // Configuración de la aplicación
 const CONFIG = {
     paypal: {
-        clientId: 'AUNiQWQRZnSr3KohXOJHkmI3NuV8bUl7nJWD5vOhvcVjLA-e8RQAPOh1E_fWORBaha2_pZNsG4dmO-P5',
+        clientId: '
+AUNiQWQRZnSr3KohXOJHkmI3NuV8bUl7nJWD5vOhvcVjLA-e8RQAPOh1E_fWORBaha2_pZNsG4dmO-P5',
         currency: 'USD',
         environment: 'production' // Cambiar a 'production' cuando esté listo
     },
